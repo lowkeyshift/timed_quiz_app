3706c4116bf0d74ed7f3d123d4329fdd647185bf
Edwin Carrasquillo <ecarra7979@gmail.com>
2017-02-23 22:30:49 -0500

    Added subtraction questions

--- questions.py
+++ questions.py
@@ -8,6 +8,10 @@ class Add(Question):
         self.text = '{} + {}'.format(num1, num2)
         self.answer = num1 + num2
 
+class Subtraction(Question):
+    def __init__(self, num1, num2):
+        self.text = '{} - {}'.format(num1, num2)
+        self.answer = num1 - num2
 
 class Multiply(Question):
     def __init__(self, num1, num2):
